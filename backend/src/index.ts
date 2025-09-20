9cdf6f6324e32c345437b569063e69f8b18165fb
fazalkadivar21 <fazalkadivar21@gmail.com>
2025-09-20 11:02:34 +0530

    updated dependencies

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,8 @@
 import express from "express";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import connect from "./db/connect";
 
 dotenv.config();
 
@@ -10,7 +11,20 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cookieParser());
 app.use(cors());
+
 app.use(express.json());
+
+connect();
+
+import { userRouter } from "./routes/user.route";
+import { captainRouter } from "./routes/captain.route";
+import { mapRouter } from "./routes/map.route";
+import { rideRouter } from "./routes/ride.route";
+
+app.use("/users",userRouter)
+app.use("/captains",captainRouter)
+app.use("/maps",mapRouter)
+app.use("/rides",rideRouter)
 
 app.get("/", (req, res) => {
     res.send("One piece is Real!");
