57210f6001dc2fd5071ce0385518693e84b64552
fazalkadivar21 <fazalkadivar21@gmail.com>
2025-09-20 11:02:06 +0530

    add: ride functions

--- backend/src/routes/ride.route.ts
+++ backend/src/routes/ride.route.ts
@@ -1,11 +1,12 @@
 import { authCaptain,authUser } from "../middleware/auth.middleware";
 import { Router } from "express";
-import {createRide,getFare,confirmRide,startRide,endRide} from "../controllers/ride.controller"
+import {createRide,getFare,confirmRide,startRide,endRide, genrateOTP} from "../controllers/ride.controller"
 
 export const rideRouter: Router = Router()
 
 rideRouter.post("/create",authUser,createRide)
 rideRouter.get("/get-fare",authUser,getFare)
 rideRouter.post("/confirm",authCaptain,confirmRide)
+rideRouter.post("/genrate-otp",authCaptain,genrateOTP)
 rideRouter.get("/start",authCaptain,startRide)
 rideRouter.post("/end",authCaptain,endRide)
